526bc3aef8fa06448d2440876dc655cc50d23b06
Sho Sato <semib5size@gmail.com>
2019-07-21 20:05:08 +0900

    [add]initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for NotificationPusher
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+{- cabal:
+build-depends: base, haskell-gi-base, gi-gtk
+-}
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
+main :: IO ()
+main = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [ #title := "Hi there" ]
+
+  on win #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [ #label := "Click me" ]
+
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+
+  #add win button
+
+  #showAll win
+
+  Gtk.main
