788c8fe2f7c06f15bd603f5b65e497f2de984b11
Sho Sato <semib5size@gmail.com>
2019-07-21 20:54:19 +0900

    [clean]move main to run method

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,4 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
-{- cabal:
-build-depends: base, haskell-gi-base, gi-gtk
--}
-
-import qualified GI.Gtk as Gtk
-import Data.GI.Base
+import Lib
 
 main :: IO ()
-main = do
-  Gtk.init Nothing
-
-  win <- new Gtk.Window [ #title := "Hi there" ]
-
-  on win #destroy Gtk.mainQuit
-
-  button <- new Gtk.Button [ #label := "Click me" ]
-
-  on button #clicked (set button [ #sensitive := False,
-                                   #label := "Thanks for clicking me" ])
-
-  #add win button
-
-  #showAll win
-
-  Gtk.main
+main = run
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
 module Lib
-    ( someFunc
+    ( run
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
+run :: IO ()
+run = do
+  Gtk.init Nothing
+  win <- new Gtk.Window [ #title := "Hi there" ]
+  on win #destroy Gtk.mainQuit
+  button <- new Gtk.Button [ #label := "Click me" ]
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+  #add win button
+  #showAll win
+  Gtk.main
